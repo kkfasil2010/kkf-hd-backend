52f09fec483074f843f3d1c1a26d810a14f26f3c
KKF <kkfasil2010@gmail.com>
2026-09-17 08:59:08 +0300

    Add hosted Privacy Policy page

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.static('public'));
+
 // Import Routes
 const indexRoute = require('./routes/indexRoute');
 const whatsappRoute = require('./routes/whatsappRoute');
